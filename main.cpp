72e84b7b949c9c511888e4df3513b3d5805e6f8e
Lacey Pevey <7490308+pevey@users.noreply.github.com>
2023-06-09 02:39:07 -0500

    add class

--- main.cpp
+++ main.cpp
@@ -1,4 +1,54 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+enum class OrderBookType {bid, ask};
+
+class OrderBookEntry {
+	public:
+		OrderBookEntry(double _price, double _amount, std::string _timestamp, std::string _product, OrderBookType _orderType)
+			: price(_price), amount(_amount), timestamp(_timestamp), product(_product), orderType(_orderType) {}
+
+		double price;
+		double amount;
+		std::string timestamp;
+		std::string product;
+		OrderBookType orderType;
+};
+
+double computeAveragePrice(std::vector<OrderBookEntry>& orders) {
+	double total = 0.0;
+	for (OrderBookEntry& order : orders) {
+		total += order.price;
+	}
+	return total / orders.size();
+}
+
+double computeLowPrice(std::vector<OrderBookEntry>& orders) {
+	double lowPrice = orders[0].price;
+	for (OrderBookEntry& order : orders) {
+		if (order.price < lowPrice) {
+			lowPrice = order.price;
+		}
+	}
+	return lowPrice;
+}
+
+double computeHighPrice(std::vector<OrderBookEntry>& orders) {
+	double highPrice = orders[0].price;
+	for (OrderBookEntry& order : orders) {
+		if (order.price > highPrice) {
+			highPrice = order.price;
+		}
+	}
+	return highPrice;
+}
+
+double computePriceSpread(std::vector<OrderBookEntry>& orders) {
+	double lowPrice = computeLowPrice(orders);
+	double highPrice = computeHighPrice(orders);
+	return highPrice - lowPrice;
+}
 
 void printMenu() {
 	 std::cout << "1: Print help" << std::endl;
@@ -69,6 +119,64 @@ void processUserOption(int userOption) {
 }
 
 int main() {
+
+	// double price = 5319.450228;
+	// double amount = 0.00020075;
+	// std::string timestamp{"2020/03/17 17:01:24.884492"};
+	// std::string product{"BTC/USDT"};
+	// enum class OrderBookType {bid, ask};
+	// OrderBookType orderType = OrderBookType::bid;
+
+	// std::vector<double> prices;
+	// std::vector<double> amounts;
+	// std::vector<std::string> timestamps;
+	// std::vector<std::string> products;
+	// std::vector<OrderBookType> orderTypes;
+
+	// prices.push_back(5319.450228);
+	// amounts.push_back(0.00020075);
+	// timestamps.push_back("2020/03/17 17:01:24.884492");
+	// products.push_back("BTC/USDT");
+	// orderTypes.push_back(OrderBookType::bid);
+
+	// std::cout << "Prices: " << prices[0] << std::endl;
+
+	// OrderBookEntry order1;
+	// order1.price = 5319.450228;
+	// order1.amount = 0.00020075;
+	// order1.timestamp = "2020/03/17 17:01:24.884492";
+	// order1.product = "BTC/USDT";
+	// order1.orderType = OrderBookType::bid;
+	// std::cout << "Price: " << order1.price << std::endl;
+
+	std::vector<OrderBookEntry> orders;
+	orders.push_back(OrderBookEntry{0.02187308, 7.44564869, "2020/03/17 17:01:24.884492", "ETH/BTC", OrderBookType::bid});
+	orders.push_back(OrderBookEntry{0.02187307, 3.467434, "2020/03/17 17:02:24.884492", "ETH/BTC", OrderBookType::bid});
+
+	double averagePrice = computeAveragePrice(orders);
+	std::cout << "Average price: " << averagePrice << std::endl;
+
+	double lowPrice = computeLowPrice(orders);
+	std::cout << "Low price: " << lowPrice << std::endl;
+
+	double highPrice = computeHighPrice(orders);
+	std::cout << "High price: " << highPrice << std::endl;
+
+	double priceSpread = computePriceSpread(orders);
+	std::cout << "Price spread: " << priceSpread << std::endl;
+
+	// for (OrderBookEntry& order : orders) {
+	// 	std::cout << "Price: " << order.price << std::endl;
+	// }
+
+	// for (unsigned int i = 0; i < orders.size(); ++i) {
+	// 	std::cout << "Price: " << orders[i].price << std::endl;
+	// }
+
+	// for (unsigned int i = 0; i < orders.size(); ++i) {
+	// 	std::cout << "Price: " << orders.at(i).price << std::endl;
+	// }
+
 	bool exit = false;
 	while (!exit) {
 		printMenu();
